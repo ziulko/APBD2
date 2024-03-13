d79084a48b224dbf9f44e351d52b1bbed9ce6e26
ziulko <abukplo@gmail.com>
2024-03-13 21:53:15 +0100

    zadanie 3

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+public class Program {
+    public static void Main(string[] args) {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia: " + average);
+    }
+
+    public static double CalculateAverage(int[] numbers) {
+        if (numbers == null || !numbers.Any()) {
+            throw new ArgumentException("Tablica liczb nie może być pusta.");
+        }
+
+        return numbers.Average();
+    }
+}
